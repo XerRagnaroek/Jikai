959431e88857eca2e0bccb748da11220fd0e29f5
XerRagnaroek <neburshizam@gmail.com>
2021-05-02 16:37:45 +0200

    fixed exceptions for embeds > max char

--- src/com/github/xerragnaroek/jikai/commands/user/SubscriptionsCommand.java
+++ src/com/github/xerragnaroek/jikai/commands/user/SubscriptionsCommand.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.github.xerragnaroek.jikai.user.JikaiUser;
+import com.github.xerragnaroek.jikai.util.BotUtils;
 
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
@@ -25,9 +26,9 @@ public class SubscriptionsCommand implements JUCommand {
 
 	@Override
 	public void executeCommand(JikaiUser ju, String[] arguments) {
-		MessageEmbed msg = ju.getSubscribedAnime().getSubscriptionsFormatted(ju);
-		if (msg != null) {
-			ju.sendPM(msg);
+		List<MessageEmbed> embeds = BotUtils.buildEmbeds(ju.getLocale().getStringFormatted("com_ju_subs_eb_title", Arrays.asList("anime"), ju.getSubscribedAnime().size()), ju.getSubscribedAnime().getSubscriptionsFormatted(ju));
+		if (!embeds.isEmpty()) {
+			embeds.forEach(e -> ju.sendPM(e));
 		} else {
 			ju.sendPM(ju.getLocale().getString("com_ju_subs_none"));
 		}
